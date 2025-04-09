2c017d59542a76dfbe9fc1b1cd745cfb57f67d6d
Valentine Wanjiru <kt116tabitha@gmail.com>
2025-04-09 09:05:11 +0300

    built a to-do application

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+	const taskForm = document.getElementById("create-task-form");
+	const taskInput = document.getElementById("new-task-description");
+	const taskString = document.getElementById("tasks");
+
+	taskForm.addEventListener("submit", (event) => {
+	  event.preventDefault();
+
+	  const text = taskInput.value;
+
+	  if (text !== "") {
+
+		const newInput = document.createElement("p");
+		newInput.textContent = text;
+
+		taskString.appendChild(newInput);
+
+		taskInput.value = "";
+	  }
+	});
+  });
+
+
+
+
